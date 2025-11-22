d4c7cf4cc307b958f5a3c1a57143a04c2fbbb7c3
Tony Nguyen <46512336+toannguyen112@users.noreply.github.com>
2025-11-22 18:36:24 +0700

    Remove stylesheet link from widget.js

--- widget.js
+++ widget.js
@@ -1,4 +1,1 @@
-const link = document.createElement('link');
-link.rel = 'stylesheet';
-link.href = 'https://cdn.jsdelivr.net/gh/Betech-JSC/bechatai-widget@latest/widget.css';
-document.head.appendChild(link);
+
